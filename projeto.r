37e819ed9dc2666c9e653882c43fa283102119cf
Juliano Cezar Teles Vaz <jctv@cin.ufpe.br>
2018-11-20 14:19:30 -0200

    Base para projeto.

---
+++ projeto.r
@@ -0,0 +1,52 @@
+#####################################################################################################
+# Universidade Federal de Pernambuco - UFPE                                                         #
+# Graduação em Ciência da Computação - Em: Novembro/2018                                            #
+# Disciplina: Estatística E Probabilidade para Computação - ET586  - Profa. Dr. Renata Souza (rmcrs)#
+# Discente: Juliano Cezar Teles Vaz (jctv)                                                          #
+# Discente: João Victor da S. Lobão (jvsl2)                                                         #
+#####################################################################################################
+####################################      PROJETO II     ############################################
+#####################################################################################################
+
+
+# Início da questão (1) ---------------------------------
+# Descarregue o arquivo .csv da planilha e imprima o dataframe obtido exatamente do jeito que ele se encontra.
+# Nessa questão, o problema da média foi resolvido da seguinte forma (...)
+
+# Início da questão (2) ---------------------------------
+# Encontre a média das notas (sem utilizar a função pronta do R).
+# Nessa questão, o problema da média foi resolvido da seguinte forma (...)
+
+
+# Início da questão (3) ---------------------------------
+# Encontre o desvio padrão das notas (sem utilizar a função pronta do R).
+# Nessa questão, o problema da média foi resolvido da seguinte forma (...)
+
+# Início da questão (4) ---------------------------------
+# Encontre a moda das notas (sem utilizar a função pronta do R).
+# Nessa questão, o problema da média foi resolvido da seguinte forma (...)
+
+
+# Início da questão (5) ---------------------------------
+# Faça uma função que retorna apenas os nomes dos filmes que possuem notas maiores ou iguais a seis (6).
+# Nessa questão, o problema da média foi resolvido da seguinte forma (...)
+
+
+# Início da questão (6) ---------------------------------
+#Faça uma função que retorna quantos filmes possuem notas abaixo de seis (6).
+# Nessa questão, o problema da média foi resolvido da seguinte forma (...)
+
+
+# Início da questão (7) ---------------------------------
+# Faça uma função que retorna o nome do filme com menor pontuação e o nome do filme com maior pontuação, nessa ordem. Por fim, faça um dataframe com os dois filmes encontrados com as colunas TÍTULO, NOTA, ANO assim como o original.
+# Nessa questão, o problema da média foi resolvido da seguinte forma (...)
+
+
+# Início da questão (8) ---------------------------------
+# Faça uma função que retorne o ano em que saíram mais filmes com notas maiores ou iguais a seis e meio (6,5).
+# Nessa questão, o problema da média foi resolvido da seguinte forma (...)
+
+
+# Início da questão (9) ---------------------------------
+# Faça um histograma onde mostra a frequência de filmes com notas maiores ou iguais a seis de cada ano. Não esqueça de dar um título e fazer ele de forma colorida, facilitando a visualização. 
+# Nessa questão, o problema da média foi resolvido da seguinte forma (...)
